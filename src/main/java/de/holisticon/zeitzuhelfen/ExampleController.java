071ff148a2705388de33acc5f33c1eb971f2aee5
Stefan Zilske <stefan.zilske@holisticon.de>
2018-10-26 13:24:28 +0200

    Add RequestMapping

--- src/main/java/de/holisticon/zeitzuhelfen/ExampleController.java
+++ src/main/java/de/holisticon/zeitzuhelfen/ExampleController.java
@@ -1,9 +1,11 @@
 package de.holisticon.zeitzuhelfen;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("api/private")
+@RestController
+@RequestMapping("api")
 public class ExampleController {
 
     @GetMapping("tryOut")
