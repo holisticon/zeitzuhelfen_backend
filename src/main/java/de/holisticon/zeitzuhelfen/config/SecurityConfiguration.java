a38aff48a057e010cff95e96e0bcfb31176d54cd
Leon Fausten <leon.fausten@holisticon.de>
2018-10-26 13:54:14 +0200

    add cors location

--- src/main/java/de/holisticon/zeitzuhelfen/config/SecurityConfiguration.java
+++ src/main/java/de/holisticon/zeitzuhelfen/config/SecurityConfiguration.java
@@ -28,7 +28,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:4200"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "OPTIONS"));
         configuration.setAllowCredentials(true);
         configuration.addAllowedHeader("Authorization");
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
